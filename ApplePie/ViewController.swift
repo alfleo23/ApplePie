b09bac975afe23b6564a9d8039004628c0b66044
alfleo23 <alonsoleone1@tiscali.it>
2019-03-02 15:58:02 +0000

    final changes

--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -18,8 +18,16 @@ class ViewController: UIViewController {
     var listOfWords = ["cazz", "bubbu"]
     var incorrectMovesAllowed = 7
     
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     
     var currentGame: Game!
     
@@ -34,13 +42,38 @@ class ViewController: UIViewController {
         let letterString = sender.title(for: .normal)!
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+
+    func updateGameState(){
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        }
+        else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        }
+        else {
+            updateUI()
+        }
     }
     
-    
     func newRound(){
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        }
+        else {
+            enableLetterButtons(false)
+        }
+        
+    }
+
+    func enableLetterButtons(_ enable: Bool){
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
     }
     
     func updateUI(){
